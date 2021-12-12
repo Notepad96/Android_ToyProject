62fffd39257e2196769ab074442266f5dea8c3e7
Notepad96 <notepad1029@gmail.com>
2021-12-12 13:18:28 +0900

    item delete 50%

--- NotepadList/app/src/main/java/com/example/notepadlist/MainListAdapter.kt
+++ NotepadList/app/src/main/java/com/example/notepadlist/MainListAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.notepadlist
 
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
@@ -7,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.mainlist_item.view.*
+import kotlinx.android.synthetic.main.mainlist_item_long_dialog.view.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -29,6 +31,22 @@ class MainListAdapter(private val list: List<Note>?): RecyclerView.Adapter<MainL
             intent.putExtra("mode", 1)
             intent.putExtra("id", list!![position].id)
             holder.layout.context.startActivity(intent)
+        }
+
+        holder.layout.vLayoutNoteListItem.setOnLongClickListener {
+            var layout = LayoutInflater.from(holder.layout.context).inflate(R.layout.mainlist_item_long_dialog, null)
+            var build = AlertDialog.Builder(holder.layout.context).apply {
+                setView(layout)
+            }
+            var dialog = build.create()
+            dialog.show()
+            layout.vTextNoteListItemDiaTitle.setText(list!![position].title)
+
+            layout.vTextNoteListItemDiaRemove.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            true
         }
     }
 
